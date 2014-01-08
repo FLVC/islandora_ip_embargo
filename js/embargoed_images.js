e6e5bec2acab15df4758d33f0e7b65c60310bafd
Randy Fischer <rf@ufl.edu>
2014-01-08 16:46:23 -0500

    fixed permissions, change overlay text and style

--- js/embargoed_images.js
+++ js/embargoed_images.js
@@ -1,6 +1,5 @@
 // Make embargoed thumbnails obvious.
 jQuery(document).ready(function($) {
   $('.islandora_ip_embargo_embargoed').wrap('<div style="position:relative;"></div>');
-  $('.islandora_ip_embargo_embargoed').after('<span style="position:absolute;' +
-    'left:0em;text-decoration:none;color:red;font-size:1.5em">EMBARGOED</span>');
+  $('.islandora_ip_embargo_embargoed').after('<span id="embargo_overlay">LIMITED ACCESS<br>Click For More Info</span>');
 });
